c8e70a8d28e8e4043cc5c9be37d93e25488e5982
Swiftyfast <jdavidswift@gmail.com>
2021-01-25 13:51:22 -0600

    init commit

---
+++ LMSProject/LMSProject.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LMSProject.UI.MvcApplication" Language="C#" %>
---
+++ LMSProject/LMSProject.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LMSProject.UI.Startup))]
+namespace LMSProject.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
